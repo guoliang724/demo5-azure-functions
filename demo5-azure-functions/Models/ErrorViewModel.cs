22164cf93f4d2e13737ac227a62d8d707a7b4bd4
Leon Zhang <leonzhang.developer@gmail.com>
2024-11-14 22:32:38 -0700

    Add project files.

---
+++ demo5-azure-functions/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace demo5_azure_functions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
